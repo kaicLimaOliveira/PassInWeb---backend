f240aa814ac0dbdec16a1a8da69d7d81d0384876
Kaic de Lima Oliveira <oliveira.kaic@hcosta.com.br>
2024-04-02 13:17:56 -0300

    first commit

---
+++ PassIn.Application/UseCases/Events/Register/RegisterEventUseCase.cs
@@ -0,0 +1,30 @@
+﻿
+using PassIn.Communication.Requests;
+using PassIn.Exceptions;
+
+namespace PassIn.Application.UseCases.Events.Register;
+public class RegisterEventUseCase
+{
+    public void Execute(RequestEventJson request)
+    {
+        Validate(request);
+    }
+
+    private void Validate(RequestEventJson request)
+    {
+        if (request.MaximumAttendees <= 0)
+        {
+            throw new PassInException("O máximo de participantes é invalido.");
+        }
+
+        if (string.IsNullOrWhiteSpace(request.Title))
+        {
+            throw new PassInException("O título é invalido.");
+        }
+
+        if (string.IsNullOrWhiteSpace(request.Details))
+        {
+            throw new PassInException("Os detalhes são invalido.");
+        }
+    }
+}
